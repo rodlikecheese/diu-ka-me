2c5ab572a433528e0d352fc8cf4cfddf658dc16b
rodlikecheese <115515066+rodlikecheese@users.noreply.github.com>
2022-10-18 10:09:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 basic.forever(function () {
-	
+    if (input.isGesture(Gesture.Shake)) {
+        pins.analogWritePin(AnalogPin.P1, 1)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P1, 1)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 80) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
 })
